009f56a3677478a5c56abc7047d7ab319dff505e
hungryDev399 <mohammedabdelaziz399@gmail.com>
2023-04-30 07:05:25 +0300

    Makin the asynchronous for loop use stack allocated arrays instead of vectors

--- OOP_project/useful tools.cpp
+++ OOP_project/useful tools.cpp
@@ -9,7 +9,17 @@
 //std::atomic<bool> working(true);
 
 int fun1() {
-	std::cout << "Hello 2\n";
+	std::cout << "Hello 1\n";
+	return 0;
+}
+
+int fun3() {
+	std::cout << "Hello 3\n";
+	return 0;
+}
+
+int fun4() {
+	std::cout << "Hello 4\n";
 	return 0;
 }
 
@@ -23,67 +33,59 @@ int* fun2(int num,int num2) {
 
 //fix void problems
 
-template <typename T, typename... Args>
- std::vector<T> asyncFor(int itrerations, dynamicFunctionPointer<T, Args...> func_ptr_in, Args... args) {
-	DynamicFutureVector<std::conditional_t<std::is_same_v<T, void>, T, std::decay_t<T>>> futures;
-	//DynamicFutureVector<T> futures;
+template <typename T, size_t itrerations, typename... Args>
+void asyncFor(T* results, dynamicFunctionPointer<T, Args...> func_ptr_in, Args... args) {
+	//DynamicFutureVector<std::conditional_t<std::is_same_v<T, void>, T, std::decay_t<T>>> futures;
+	DynamicFutureVector<T> futures;
 	// reserving the numebr of returns to avoid coppying 
 	futures.generated_vector.reserve(itrerations);
-	for (int i = 0; i <= itrerations;i++) {
+	for (int i = 0; i < itrerations;i++) {
 		futures.generated_vector.push_back(std::async(std::launch::async, func_ptr_in.func_pointer, args...));
 	}
-	
+
 	//ensure that all async tasks are done
-	std::vector<T> results;
-	results.reserve(itrerations);
-	for (auto& future : futures.generated_vector) {
-		
-		results.push_back(future.get());
-		
+	for (int i = 0; i < futures.generated_vector.size(); i++) {
+		results[i] = (futures.generated_vector[i].get());
+
 	}
-	return results;
+
+
+
 }
+ 
 
 
- /*template<typename T>
- void print(T& T) {
-	 cout << *T;
- }
- */
+int maintest() {
+	const int iterations = 8000;
+
+	int returns[iterations];
+	{
+		objectLifetimeTimer timer;
+		asyncFor<int, iterations>(returns, dynamicFunctionPointer<int>{&fun1});
+
+	}
+	std::cin.get();
 
 
- int main10() {
-	 const int iterations = 8000;
-		std::vector<int> returns;
-	 {
-		 objectLifetimeTimer timer;
-		//dynamicFunctionPointer<int> fun2_ptr = { &fun1};
-		//returns = asyncFor(8000, fun2_ptr);
-		 auto returns = asyncFor<int>(iterations, dynamicFunctionPointer<int>{&fun1});
-		//dynamicFunctionPointer<int> fun2_ptr = { &fun1 };
-		//returns = asyncFor(70000, fun2_ptr);
- }
-	 std::cin.get();
 
-	 {
-		 objectLifetimeTimer timer;
-		 std::vector<int> returns;
-		 std::array<int, (iterations)> returns2;
-		 for (int i = 0; i < 8000;i++) {
-			 std::cout << "Hello 2\n";
-			 returns2[i] = 0;
-			 //int* arr = new int[2];
-			 //arr[0] = 5;
-			 //arr[1] = 10;
-			 //returns.push_back(arr);
-		 }
-	 }
+	{
+		objectLifetimeTimer timer;
+		std::array<int, (iterations)> returns3;
+		for (int i = 0; i < 8000;i++) {
+			std::cout << "Hello 2\n";
+			returns3[i] = 0;
+			//int* arr = new int[2];
+			//arr[0] = 5;
+			//arr[1] = 10;
+			//returns.push_back(arr);
+		}
+	}
 	//std::cout << returns[0][1];
 	//delete the heap allocated pointers
 	//for (auto ptr : returns) {
 		//delete[] ptr;
 	//}
-	
-	 
+
+
 	return 0;
 }

fd95e1997389d682f061caebb9392b9205fe2c8a
mohamedtag04 <mohamedtag264@gmail.com>
2023-05-21 01:40:53 +0300

    seperated product & supplier header files from cpp

--- OOP_project/product_tag.h
+++ OOP_project/product_tag.h
@@ -1,106 +1,44 @@
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
-#include <iostream>
-#include <string>
-#include <vector>
 #include <random>
+#include "Product.h"
 
 using namespace std;
 
 class PRODUCT {
 public:
-    // constructors
-    PRODUCT() {
-        this->code = 0;
-        this->name = "";
-        this->quantity = 0;
-        this->product_supplier_id = 0;
-        this->is_pizza = false;
-        this->price = 0;
-    }
-    // constructor
-    PRODUCT(string name, int quantity, int product_supplier_id, int price, bool is_pizza) {
-        this->name = name;
-        this->quantity = quantity;
-        this->product_supplier_id = product_supplier_id;
-        this->is_pizza = is_pizza;
-        this->price = price;
-        generate_code();
-    }
+    PRODUCT();
+    PRODUCT(string name, int quantity, int product_supplier_id, int price, bool is_pizza);
 
     // getters
-    int get_code() {
-        return this->code;
-    }
-    int get_quantity() {
-        return this->quantity;
-    }
-    string get_name() {
-        return this->name;
-    }
-    int get_product_supplier_id() {
-        return this->product_supplier_id;
-    }
-    bool get_is_pizza() {
-        return this->is_pizza;
-    }
-    int get_price() {
-        return this->price;
-    }
+    int get_code();
+    int get_quantity();
+    string get_name();
+    int get_product_supplier_id();
+    bool get_is_pizza();
+    int get_price();
 
     // setters
-    void set_code(int code) {
-        this->code = code;
-    }
-    void set_name(string name) {
-        this->name = name;
-    }
-    void set_quantity(int quantity) {
-        this->quantity = quantity;
-    }
-    void set_product_supplier_id(int product_supplier_id) {
-        this->product_supplier_id = product_supplier_id;
-    }
-    void set_is_pizza(bool is_pizza) {
-        this->is_pizza = is_pizza;
-    }
-    void set_price(int price) {
-        this->price = price;
-    }
+    void set_code(int code);
+    void set_name(string name);
+    void set_quantity(int quantity);
+    void set_product_supplier_id(int product_supplier_id);
+    void set_is_pizza(bool is_pizza);
+    void set_price(int price);
 
-    // other functions
-    void print_product() {
-        cout << "Product code: " << this->code << endl;
-        cout << "Product name: " << this->name << endl;
-        cout << "Product quantity: " << this->quantity << endl;
-        cout << "Product supplier id: " << this->product_supplier_id << endl; // connect to supplier
-        cout << "Product is pizza: " << this->is_pizza << endl;
-    }
+    // methods
+    void generate_code();
+    void print_product();
 
-    void generate_code() {
-        random_device rd;
-        mt19937 gen(rd());
-        uniform_int_distribution<> distrib(100000, 999999);
-        int new_code = distrib(gen);
-        this->code = new_code;
-    }
-
-    void add_quantity(int quantity) {
-        this->quantity += quantity;
-    }
-
-    void remove_quantity(int quantity) {
-        this->quantity -= quantity;
-    }
-
-private:
-    int code;
-    string name;
-    int quantity;
-    int product_supplier_id;
-    bool is_pizza;
-    int price;
+    private:
+        int code;
+		string name;
+		int quantity;
+		int product_supplier_id;
+		bool is_pizza;
+		int price;
 };
 
 #endif // PRODUCT_H
+
--- OOP_project/supplier_tag.h
+++ OOP_project/supplier_tag.h
@@ -6,89 +6,38 @@
 #ifndef SUPPLIER_H
 #define SUPPLIER_H
 
-#include <iostream>
-#include <string>
 #include "product.h"
-
-using namespace std;
+#include "Supplier.h"
 
 class supplier {
 public:
-    supplier(int supplier_id, string name, string address, string phone_number, string email) {
-        this->supplier_id = supplier_id;
-        this->name = name;
-        this->address = address;
-        this->phone_number = phone_number;
-        this->email = email;
-    }
-    supplier() {
-        supplier_id = 0;
-        name = "";
-        address = "";
-        phone_number = "";
-        email = "";
-    }
-
+    supplier();
+    supplier(int supplier_id, string name, string address, string phone_number, string email);
 
     // getters
-    int get_supplier_id() {
-        return this->supplier_id;
-    }
-
-    string get_name() {
-        return this->name;
-    }
-
-    string get_address() {
-        return this->address;
-    }
-
-    string get_phone_number() {
-        return this->phone_number;
-    }
-
-    string get_email() {
-        return this->email;
-    }
+    int get_supplier_id();
+    string get_name();
+    string get_address();
+    string get_phone_number();
+    string get_email();
 
     // setters
-    void set_supplier_id(int supplier_id) {
-        this->supplier_id = supplier_id;
-    }
-
-    void set_name(string name) {
-        this->name = name;
-    }
-
-    void set_address(string address) {
-        this->address = address;
-    }
-
-    void set_phone_number(string phone_number) {
-        this->phone_number = phone_number;
-    }
-
-    void set_email(string email) {
-        this->email = email;
-    }
+    void set_supplier_id(int supplier_id);
+    void set_name(string name);
+    void set_address(string address);
+    void set_phone_number(string phone_number);
+    void set_email(string email);
 
     // other functions
-    void print_supplier() {
-        cout << "Supplier ID: " << this->supplier_id << endl;
-        cout << "Supplier name:" << this->name << endl;
-        cout << "Supplier address: " << this->address << endl;
-        cout << "Supplier phone number: " << this->phone_number << endl;
-        cout << "Supplier email: " << this->email << endl;
-    }
+    void print_supplier();
 
 private:
     int supplier_id;
-    string name;
-    string address;
-    string phone_number;
-    string email;
-
-
+	string name;
+	string address;
+	string phone_number;
+	string email;
 };
+
 #endif // SUPPLIER_H
 
